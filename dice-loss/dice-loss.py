164e193f0539ee5b5609732b4831ad7f07bcecd5
Raj Kumar <raj6713631@gmail.com>
2026-04-12 15:58:37 +0530

    Add solution for dice-loss

---
+++ dice-loss/dice-loss.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def dice_loss(p, y, eps=1e-8):
+    """
+    Compute Dice Loss for segmentation.
+    """
+    p = np.array(p).flatten()
+    y = np.array(y).flatten()
+    numerator = 2*np.sum(p*y)+eps
+    denominator = (np.sum(p)+np.sum(y)+eps)
+    dice = numerator/denominator
+    return 1-dice
