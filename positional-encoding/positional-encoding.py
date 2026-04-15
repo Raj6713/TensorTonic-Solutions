fc5cb4a4ad0992464bf2828bb791fbb89e860146
Raj Kumar <raj6713631@gmail.com>
2026-04-15 22:43:17 +0530

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -5,13 +5,14 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Return PE of shape (seq_len, d_model) using sin/cos formulation.
     Odd d_model -> last column is sin.
     """
-    pos = np.arange(seq_len)[:, np.newaxis]
-    i = np.arange(d_model)[np.newaxis,:]
-    angle_rates = 1 / np.power(base, (2 * (i // 2)) / d_model)
-    angles = pos*angle_rates
-    print(angles)
-    pe = np.zeros((seq_len, d_model))
-    pe[:, 0::2] = np.sin(angles[:, 0::2])            
-    pe[:, 1::2] = np.cos(angles[:, 1::2])            
-    return pe
+    output = np.zeros((seq_len, d_model))
+    rows = np.arange(seq_len)[:,np.newaxis]
+    columns = np.arange(d_model)[np.newaxis,:]
+    angle_rates = 1 / np.power(base, (2 * (columns // 2)) / d_model)
+    angle = rows*angle_rates
+    print(angle.shape, output.shape)
+
+    output[:,0::2] = np.sin(angle[:, 0::2])
+    output[:,1::2] = np.cos(angle[:, 1::2])
+    return output
     
