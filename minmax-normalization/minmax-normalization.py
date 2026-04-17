2aad13b43a6a4eb10481f395a1786a35a8a9b697
Raj Kumar <raj6713631@gmail.com>
2026-04-17 22:32:03 +0530

    Update solution for minmax-normalization

--- minmax-normalization/minmax-normalization.py
+++ minmax-normalization/minmax-normalization.py
@@ -5,12 +5,12 @@ def minmax_scale(X, axis=0, eps=1e-12):
     Scale X to [0,1]. If 2D and axis=0 (default), scale per column.
     Return np.ndarray (float).
     """
-    X = np.asarray(X, dtype=float)
-    X_min = np.min(X, axis=axis,keepdims=True)
-    X_max = np.max(X, axis=axis,keepdims=True)
-    print(X_min, X_max)
-    denom = X_max-X_min
-    denom = np.where(denom<eps,1.0,denom)
-    X_scaled = (X - X_min)/denom
-    return X_scaled
-    pass
+    X = np.asarray(X)
+    x_min = np.min(X, axis=axis, keepdims=True)
+    x_max = np.max(X, axis=axis, keepdims=True)
+    print(X.shape, x_max.shape, x_min.shape)
+    denom = x_max-x_min
+    denom = np.maximum(denom, eps)
+    X = (X-x_min)/denom
+    return X
+    
