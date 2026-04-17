8f0c375419d07f0f6819c2a45be247e7aad7172f
Raj Kumar <raj6713631@gmail.com>
2026-04-18 01:49:55 +0530

    Update solution for manhattan-distance

--- manhattan-distance/manhattan-distance.py
+++ manhattan-distance/manhattan-distance.py
@@ -1,10 +1,11 @@
 import numpy as np
+import torch
 
 def manhattan_distance(x, y):
     """
     Compute the Manhattan (L1) distance between vectors x and y.
     Must return a float.
     """
-    x = np.asarray(x, dtype=float)
-    y = np.asarray(y, dtype=float)
-    return np.sum(np.abs(x-y))
+    x = torch.as_tensor(x, dtype=torch.float32)
+    y = torch.as_tensor(y, dtype=torch.float32)
+    return torch.sum(torch.abs(x-y)).item()
