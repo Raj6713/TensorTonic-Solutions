01f5fed11b52f77fa111bf32cc7d39d0cd04e944
Raj Kumar <raj6713631@gmail.com>
2026-04-18 02:12:08 +0530

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -1,10 +1,14 @@
 import numpy as np
+import torch
 
 def dot_product(x, y):
     """
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
     """
+    # x = torch.as_tensor(x, dtype=torch.float32)
+    # y = torch.as_tensor(y, dtype=torch.float32)
+    # return torch.dot(x,y).item()
     x = np.asarray(x, dtype=float)
     y = np.asarray(y, dtype=float)
     return np.dot(x,y)
